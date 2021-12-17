9713e3b0409d7c3cf89a1a5b73f0c33558d34504
Dustin David Meyer <dustin.m@kiwies.com>
2021-12-17 10:42:00 +0100

    Add Title Page

--- Flüchtlingsbewegung-Webpage/src/App.js
+++ Flüchtlingsbewegung-Webpage/src/App.js
@@ -1,10 +1,14 @@
 import "./files/css/index.scss"
 import Header from "./modules/header";
+import TitlePage from "./modules/TitlePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <TitlePage />
+      </div>
     </div>
   );
 }
---
+++ Flüchtlingsbewegung-Webpage/src/modules/TitlePage.js
@@ -0,0 +1,12 @@
+import "../files/css/TitlePage.scss"
+
+const TitlePage = () => {
+    return ( <div className="TitlePage">
+        <div className="title">
+        <h1>Die Auswanderung nach Amerika</h1>
+        <h3>Eine Ausarbeitung von Iven, Lukas, Arman und Dustin</h3>
+        </div>
+    </div> );
+}
+ 
+export default TitlePage;
