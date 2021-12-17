e43dbb86f6a3ffb27193a43fc1d21a1e202c3996
Dustin David Meyer <dustin.m@kiwies.com>
2021-12-17 09:11:09 +0100

    add basics and header

---
+++ Flüchtlingsbewegung-Webpage/src/modules/header.js
@@ -0,0 +1,13 @@
+import "../files/css/header.scss"
+
+const Header = () => {
+    return ( <header>
+        <div className="logo">
+            <h1>Flüchtlingsbewegung</h1>
+            <h3 className="bindestrich"> - </h3>
+            <h3>Die Flucht aus Europa</h3>
+        </div>
+    </header> );
+}
+ 
+export default Header;
